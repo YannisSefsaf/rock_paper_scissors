1c79ea60a40890a98f9443d4ba6b289342f90074
YannisSefsaf <111019910+YannisSefsaf@users.noreply.github.com>
2022-11-03 15:09:45 -0600

    Improved UI

--- javascript.js
+++ javascript.js
@@ -1,26 +1,96 @@
+// VARIABLES //
+let computerChoice = ["Rock", "Scissors", "Paper"];
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+// QUERY SELECTORS //
+const options = document.querySelectorAll(".option");
+const startGame = document.querySelector(".start__game");
+const showPlayerScore = document.querySelector(".player__score");
+const showComputerScore = document.querySelector(".computer__score");
+const roundResult = document.querySelector(".round__result");
+const playto = document.querySelector("#playto");
+const result = document.querySelector(".result");
+let maxScore = Number(playto.value);
+showPlayerScore.innerText = `${scorePlayer}`;
+showComputerScore.innerText = `${scoreComputer}`;
+
+// EVENT LISTENERS //
+options.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    game(e.target.innerText);
+  });
+});
+
+startGame.addEventListener("click", function (e) {
+  scorePlayer = 0;
+  scoreComputer = 0;
+  keepScore(scorePlayer, scoreComputer);
+  result.innerText = "";
+});
+
+playto.addEventListener("click", (e) => {
+  maxScore = Number(playto.value);
+});
+
+// FUNCTIONS //
 function getComputerChoice() {
-  let num = Math.random();
-  if (num <= 1 / 3) {
-    return "Rock";
-  } else if (num > 2 / 3) {
-    return "Scissors";
-  } else {
-    return "Paper";
-  }
-  console.log(getComputerChoice);
+  return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (
-    (playerSelection.toLowerCase() === "rock" &&
-      computerSelection.toLowerCase() === "rock") ||
-    (playerSelection.toLowerCase() === "paper" &&
-      computerSelection.toLowerCase() === "paper") ||
-    (playerSelection.toLowerCase() === "scissors" &&
-      computerSelection.toLowerCase() === "scissors")
-  ) {
+  if (isDraw(playerSelection, computerSelection)) {
+    displayRoundResult(playerSelection, computerSelection);
     return "draw";
-  } else if (
+  } else if (isWinningPlay(playerSelection, computerSelection)) {
+    displayRoundResult(playerSelection, computerSelection);
+    return "win";
+  } else if (isLosingPlay(playerSelection, computerSelection)) {
+    displayRoundResult(playerSelection, computerSelection);
+    return "loss";
+  }
+}
+
+function game(playerSelection) {
+  let computerSelection = getComputerChoice();
+  if (isMaxScore(scorePlayer, scoreComputer)) {
+    if (playRound(playerSelection, computerSelection) === "win") {
+      scorePlayer++;
+      keepScore(scorePlayer, scoreComputer);
+      isMaxScore(scorePlayer, scoreComputer);
+    } else if (playRound(playerSelection, computerSelection) === "loss") {
+      scoreComputer++;
+      keepScore(scorePlayer, scoreComputer);
+      isMaxScore(scorePlayer, scoreComputer);
+    }
+  } else {
+    keepScore(scorePlayer, scoreComputer);
+  }
+}
+
+function keepScore(scorePlayer, scoreComputer) {
+  showPlayerScore.innerText = `${scorePlayer}`;
+  showComputerScore.innerText = `${scoreComputer}`;
+}
+
+function displayRoundResult(playerSelection, computerSelection) {
+  if (isDraw(playerSelection, computerSelection)) {
+    roundResult.textContent = `Draw! Both of y'all chose ${
+      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    }`;
+  } else if (isWinningPlay(playerSelection, computerSelection)) {
+    roundResult.textContent = `You won! ${computerSelection} beats ${
+      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    }`;
+  } else if (isLosingPlay(playerSelection, computerSelection)) {
+    roundResult.textContent = `You lost! ${computerSelection} beats ${
+      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    }`;
+  }
+}
+
+function isWinningPlay(playerSelection, computerSelection) {
+  if (
     (playerSelection.toLowerCase() === "rock" &&
       computerSelection.toLowerCase() === "scissors") ||
     (playerSelection.toLowerCase() === "paper" &&
@@ -28,8 +98,14 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection.toLowerCase() === "scissors" &&
       computerSelection.toLowerCase() === "paper")
   ) {
-    return "win";
-  } else if (
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isLosingPlay(playerSelection, computerSelection) {
+  if (
     (playerSelection.toLowerCase() === "rock" &&
       computerSelection.toLowerCase() === "paper") ||
     (playerSelection.toLowerCase() === "paper" &&
@@ -37,54 +113,30 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection.toLowerCase() === "scissors" &&
       computerSelection.toLowerCase() === "rock")
   ) {
-    return "loss";
+    return true;
+  } else {
+    return false;
   }
 }
 
-function game(num) {
-  let scorePlayer = 0;
-  let scoreComputer = 0;
-  while (scorePlayer < num && scoreComputer < num) {
-    let playerSelection = prompt("Choose pls");
-    let computerSelection = getComputerChoice();
-    if (playRound(playerSelection, computerSelection) === "win") {
-      console.log(
-        `You won! ${
-          playerSelection[0].toUpperCase() +
-          playerSelection.slice(1).toLowerCase()
-        } beats ${computerSelection}`
-      );
-      scorePlayer++;
-      console.log(
-        `Player score: ${scorePlayer} - Computer score: ${scoreComputer}`
-      );
-    } else if (playRound(playerSelection, computerSelection) === "loss") {
-      console.log(
-        `You lost! ${computerSelection} beats ${
-          playerSelection[0].toUpperCase() +
-          playerSelection.slice(1).toLowerCase()
-        }`
-      );
-      scoreComputer++;
-      console.log(
-        `Player score: ${scorePlayer} - Computer score: ${scoreComputer}`
-      );
-    } else {
-      console.log(playRound(playerSelection, computerSelection));
-      console.log(
-        `Draw! Both of y'all chose ${
-          playerSelection[0].toUpperCase() +
-          playerSelection.slice(1).toLowerCase()
-        }`
-      );
-      console.log(
-        `Player score: ${scorePlayer} - Computer score: ${scoreComputer}`
-      );
-    }
+function isDraw(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return true;
+  } else {
+    return false;
   }
-  if (scorePlayer === num) {
-    return "You won the game!!!";
-  } else if (scoreComputer === num) {
-    return "You lost lmao";
+}
+
+function isMaxScore(scorePlayer, scoreComputer) {
+  if (scorePlayer < maxScore && scoreComputer < maxScore) {
+    return true;
+  } else if (scorePlayer >= maxScore) {
+    result.innerText = "Holy shit you won! Congrats!!!";
+    result.style.color = "green";
+    return false;
+  } else if (scoreComputer >= maxScore) {
+    result.innerText = "Damn, son! RIP";
+    result.style.color = "red";
+    return false;
   }
 }
